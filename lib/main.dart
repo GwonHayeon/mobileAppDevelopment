209abeafcd6f259db9b0a720c5e9076ee3a84652
GwonHayeon <80669922+GwonHayeon@users.noreply.github.com>
2023-10-04 13:41:13 +0900

    1004 - TabBarView

--- lib/main.dart
+++ lib/main.dart
@@ -14,7 +14,7 @@ class MyApp extends StatelessWidget {
       title: 'First Flutter App',
       theme: ThemeData(
         primaryColor: Colors.blue, //기본으로 blue로 설정. 이것만 바꾼다고 해서 색이 바뀌지않음
-        primarySwatch: Colors.orange, //이것까지 해줘야함
+        primarySwatch: Colors.blue, //이것까지 해줘야함
         //color custormaize
 
         fontFamily: 'Pretendard',
@@ -30,29 +30,46 @@ class MyHomePage extends StatelessWidget {
 
   @override
   Widget build(BuildContext context) {
-    return Scaffold(
-      appBar: AppBar(
-        backgroundColor: AppColor.Malachite,
-        title: const Text('Flutter Demo'), //const를 붙이냐 마느냐는 성능상의 문제. 불필요한 리밴더링 하지 않게 되기때문에 성능이 좋아진다
-        // leading: Icon(Icons.add),
-        actions: [
-          IconButton(onPressed: (){}, icon: Icon(Icons.add))
-        ],
+    return DefaultTabController( //탭바 쓰려면 이걸로 감싸줘야함
+      length: 3,
+      child: Scaffold(
+        appBar: AppBar(
+          backgroundColor: AppColor.Malachite,
+          title: const Text('Flutter Demo'), //const를 붙이냐 마느냐는 성능상의 문제. 불필요한 리밴더링 하지 않게 되기때문에 성능이 좋아진다
+          // leading: Icon(Icons.add),
+          actions: [
+            IconButton(onPressed: (){}, icon: Icon(Icons.add))
+          ],
+          bottom: const TabBar(
+            tabs: [
+              Tab(icon:Icon(Icons.tag_faces)),
+              Tab(text: "Menu",),
+              Tab(icon:Icon(Icons.info), text: "Info"),
+            ],
+          ),
+        ),
+        body: TabBarView(
+          children : [
+            Container(
+              color: Colors.red,
+            ),
+            Container(
+              color: Colors.green,
+            ),
+            Container(
+              color: Colors.blue,
+            ),
+          ]
+        ),
+        drawer: Drawer(),//Scaffold의 property
+        bottomNavigationBar: BottomNavigationBar(
+          items: const [
+            BottomNavigationBarItem(icon: Icon(Icons.home), label:"Home"),
+            BottomNavigationBarItem(icon: Icon(Icons.person), label:"Profile"),
+            BottomNavigationBarItem(icon: Icon(Icons.notifications), label:"Notifications"),
+          ],
+        ),
       ),
-      body: PageView(
-        children : [
-          Container(
-            color: Colors.red,
-          ),
-          Container(
-            color: Colors.green,
-          ),
-          Container(
-            color: Colors.blue,
-          ),
-        ]
-      ),
-      drawer: Drawer(),//Scaffold의 property
     );
   }
 }
