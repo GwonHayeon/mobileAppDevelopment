437c89994ea3d455b2c22fd22168bfa7820d8657
GwonHayeon <80669922+GwonHayeon@users.noreply.github.com>
2023-10-09 17:10:36 +0900

    1005 - list fx

--- lib/main.dart
+++ lib/main.dart
@@ -1,5 +1,4 @@
 import "package:flutter/material.dart";
-import 'style.dart';
 
 void main() {
   runApp(MyApp());
@@ -15,10 +14,9 @@ class MyApp extends StatelessWidget {
       title: 'First Flutter App',
       theme: ThemeData(
         primaryColor: Colors.blue, //기본으로 blue로 설정. 이것만 바꾼다고 해서 색이 바뀌지않음
-        primarySwatch: Colors.purple, //이것까지 해줘야함
+        primarySwatch: Colors.indigo, //이것까지 해줘야함
         //color custormaize
-
-        fontFamily: 'Pretendard',
+        // fontFamily: 'Pretendard',
       ),
       home: MyHomePage(),
     );
@@ -28,7 +26,7 @@ class MyApp extends StatelessWidget {
 class MyHomePage extends StatelessWidget {
   MyHomePage({super.key});
 
-  final items = List.generate(100, (index) => index).toList();
+  final items = List.generate(15, (index) => index).toList();
 
   @override
   Widget build(BuildContext context) {
@@ -37,44 +35,152 @@ class MyHomePage extends StatelessWidget {
       length: 3,
       child: Scaffold(
         appBar: AppBar(
-          backgroundColor: AppColor.Malachite,
-          title: const Text('Flutter Demo'),
-          //const를 붙이냐 마느냐는 성능상의 문제. 불필요한 리밴더링 하지 않게 되기때문에 성능이 좋아진다
-          // leading: Icon(Icons.add),
-          actions: [IconButton(onPressed: () {}, icon: Icon(Icons.add))],
+          backgroundColor: Colors.blue,
+          title: const Text('Assignment3'),
+          actions: [
+            IconButton(
+              onPressed: () {},
+              icon: Icon(Icons.add),
+            ),
+          ],
           bottom: const TabBar(
             tabs: [
-              Tab(icon: Icon(Icons.tag_faces)),
+              Tab(icon: Icon(Icons.image)),
               Tab(
-                text: "Menu",
+                icon: Icon(Icons.how_to_vote),
               ),
-              Tab(icon: Icon(Icons.info), text: "Info"),
+              Tab(icon: Icon(Icons.upload)),
             ],
           ),
         ),
         body: TabBarView(children: [
           Tab(
-            child: LinearProgressIndicator(
-              //IconButton, TextButton, elevaluationButton, FloatingActionButton
-              //CircularProgressIndicator : 로딩중
-              // onPressed: (){},
-              // child: Icon(Icons.add),
-            )
+            child: GridView.count(
+              crossAxisCount: 3,
+              children: items
+                  .map((i) => Container(
+                      child: Image(
+                          image: AssetImage(
+                              'assets/dog_images/dog_${i + 1}.jpeg'))))
+                  .toList(),
+            ),
           ),
           Container(
-            color: Colors.green,
+            child: Column(
+              children: [
+                const Padding(
+                  padding: EdgeInsets.all(8.0),
+                  child: Text(
+                    "Candidate",
+                    style: TextStyle(
+                      fontSize: 18,
+                    ),
+                  ),
+                ),
+                Row(
+                  mainAxisAlignment: MainAxisAlignment.spaceEvenly,
+                  children: [
+                    Card(
+                      shape: RoundedRectangleBorder(
+                        borderRadius: BorderRadius.circular(10.0),
+                      ),
+                      elevation: 4.0,
+                      child: Container(
+                        width: 120,
+                        height: 100,
+                        child: const Center(
+                          child: Text(
+                            'Dog1',
+                            style: TextStyle(color: Colors.red),
+                          ),
+                        ),
+                      ),
+                    ),
+                    Card(
+                      shape: RoundedRectangleBorder(
+                        borderRadius: BorderRadius.circular(10.0),
+                      ),
+                      elevation: 4.0,
+                      child: Container(
+                        width: 120,
+                        height: 100,
+                        child: const Center(
+                          child: Text(
+                            'Dog1',
+                            style: TextStyle(color: Colors.blue),
+                          ),
+                        ),
+                      ),
+                    ),
+                    Card(
+                      shape: RoundedRectangleBorder(
+                        borderRadius: BorderRadius.circular(10.0),
+                      ),
+                      elevation: 4.0,
+                      child: Container(
+                        width: 120,
+                        height: 100,
+                        child: const Center(
+                          child: Text(
+                            'Dog1',
+                            style: TextStyle(color: Colors.green),
+                          ),
+                        ),
+                      ),
+                    ),
+                  ],
+                ),
+                const Padding(
+                  padding: EdgeInsets.all(8.0),
+                  child: Text(
+                    "Vote rate",
+                    style: TextStyle(
+                      fontSize: 18,
+                    ),
+                  ),
+                ),
+                Row(
+                  children: [
+                    Expanded(
+                      flex: 5,
+                      child: Container(
+                        color: Colors.red,
+                        padding: const EdgeInsets.symmetric(vertical: 40),
+                        child: const Center(child: Text("50%")),
+                      ),
+                    ),
+                    Expanded(
+                      flex: 2,
+                      child: Container(
+                        color: Colors.blue,
+                        padding: EdgeInsets.symmetric(vertical: 40),
+                        child: const Center(child: Text("20%")),
+                      ),
+                    ),
+                    Expanded(
+                      flex: 3,
+                      child: Container(
+                        color: Colors.green,
+                        padding: EdgeInsets.symmetric(vertical: 40),
+                        child: const Center(child: Text("30%")),
+                      ),
+                    ),
+                  ],
+                )
+              ],
+            ),
           ),
-          Container(
-            color: Colors.blue,
+          Tab(
+            child: LinearProgressIndicator(),
           ),
         ]),
-        drawer: Drawer(), //Scaffold의 property
         bottomNavigationBar: BottomNavigationBar(
           items: const [
-            BottomNavigationBarItem(icon: Icon(Icons.home), label: "Home"),
-            BottomNavigationBarItem(icon: Icon(Icons.person), label: "Profile"),
             BottomNavigationBarItem(
-                icon: Icon(Icons.notifications), label: "Notifications"),
+                icon: Icon(Icons.photo_album), label: "album"),
+            BottomNavigationBarItem(icon: Icon(Icons.home), label: "home"),
+            BottomNavigationBarItem(
+                icon: Icon(Icons.settings), label: "settings"),
           ],
         ),
       ),
--- lib/style.dart
+++
@@ -1,6 +0,0 @@
-import 'package:flutter/material.dart';
-
-class AppColor extends Color{
-  AppColor(super.value);
-  static const Malachite = Color(0xFF58B906); //0xFF + 코드
-}
