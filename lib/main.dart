428a4c361ee91dc847754bedda8055108aae6797
GwonHayeon <80669922+GwonHayeon@users.noreply.github.com>
2023-10-19 00:01:46 +0900

    1018 - use key

--- lib/main.dart
+++ lib/main.dart
@@ -1,7 +1,5 @@
-import 'dart:math';
 import "package:flutter/material.dart";
-
-enum Language { cpp, python, dart }
+enum Team { leader, member }
 
 void main() {
   runApp(MyApp());
@@ -9,11 +7,11 @@ void main() {
 
 class MyApp extends StatelessWidget {
   MyApp({super.key});
+
   @override
   Widget build(BuildContext context) {
     return MaterialApp(
-      //const제거
-      title: 'Receive User Information',
+      title: 'My Home Page',
       theme: ThemeData(
         primarySwatch: Colors.purple, //이것까지 해줘야함
       ),
@@ -31,50 +29,202 @@ class MyHomePage extends StatefulWidget {
 
 class _MyHomePageState extends State<MyHomePage> {
   List items = List.generate(20, (i) => i);
+
+
   @override
   Widget build(BuildContext context) {
     return Scaffold(
-        appBar: AppBar(
-          title: const Text("My Home Page"),
-          actions: [
-            IconButton(
-              onPressed: () {},
-              icon: const Icon(
-                Icons.add,
-              ),),
-          ],
-        ),
-        body: ReorderableListView.builder(
-            itemCount: items.length,
-            itemBuilder:(c,i){
-              return Dismissible(
-                background: Container(color: Colors.green,),
-                key: ValueKey(items[i]),
-                child: ListTile(
-                  title: Text("Student ${items[i]}"),
-                  leading : const Icon(Icons.home),
-                  trailing: const Icon(Icons.navigate_next),
-                ),
-                onDismissed: (direction){
-                  setState(() {
-                    items.removeAt(i);
-                    print(items);
-                  });
-                },
-              );
-            },
-            onReorder: (int oldIndex, int newIndex){
-                setState(() {
-                  if(oldIndex < newIndex){
-                    newIndex -= 1;
-                  }
-                  items.insert(newIndex, items.removeAt(oldIndex));
-                  print(items);
-                });
-          },
-        )
+      appBar: AppBar(
+        title: const Text("My Home Page"),
+        actions: [
+          IconButton(
+            onPressed: () {},
+            icon: const Icon(
+              Icons.add,
+            ),
+          ),
+        ],
+      ),
+      body: MyForm(),
     );
   }
 }
 
+class MyForm extends StatefulWidget {
+  const MyForm({super.key});
 
+  @override
+  State<MyForm> createState() => _MyFormState();
+}
+
+class _MyFormState extends State<MyForm> {
+  StudentResult studentResult = StudentResult(0, 0, 0, 0, true);
+
+  final _formKey = GlobalKey<FormState>();
+  final _midtermController = TextEditingController();
+  final _finalController = TextEditingController();
+  String _grade = " ";
+  Team _team = Team.leader;
+  final _valueList = List.generate(10, (index) => "$index point");
+  var _selectedValue = '0 point';
+  bool _isAbsense = false;
+
+  @override
+  Widget build(BuildContext context) {
+    return Padding(
+      padding: const EdgeInsets.all(16.0),
+      child: Form(
+          key: _formKey,
+          child: ListView(
+            children: [
+              TextFormField(
+                decoration: const InputDecoration(
+                  border: OutlineInputBorder(),
+                  labelText: 'Mid-term exam',
+                ),
+                validator: (value) {
+                  if (value == null || value.isEmpty) {
+                    return "Insert some text";
+                  } else if (int.tryParse(value) == null) {
+                    return 'Insert some integer';
+                  }
+                  return null;
+                },
+                onSaved: (value) {
+                  studentResult.midTermExam = int.parse(value!);
+                },
+              ),
+              const SizedBox(
+                height: 20,
+              ),
+              TextFormField(
+                decoration: const InputDecoration(
+                  border: OutlineInputBorder(),
+                  labelText: 'Final-term exam',
+                ),
+                validator: (value) {
+                  if (value == null || value.isEmpty) {
+                    return "Insert some text";
+                  } else if (int.tryParse(value) == null) {
+                    return 'Insert some integer';
+                  }
+                  return null;
+                },
+                onSaved: (value) {
+                  studentResult.finalTermExam = int.parse(value!);
+                },
+              ),
+              const SizedBox(
+                height: 20,
+              ),
+              DropdownButtonFormField(
+                decoration: const InputDecoration(
+                    border: OutlineInputBorder(),
+                    labelText: 'Additional Point'),
+                value: studentResult.additionalPoint,
+                items: List.generate(
+                  11,
+                  (i) {
+                    if (i == 0) {
+                      return DropdownMenuItem(
+                        value: i,
+                        child: const Text('Choose the additioinal point'),
+                      );
+                    }
+                    return DropdownMenuItem(
+                      value: i,
+                      child: Text('${i - 1} point'),
+                    );
+                  },
+                ),
+                onChanged: (value) {
+                  setState(() {
+                    studentResult.additionalPoint = value!;
+                  });
+                },
+                validator: (value) {
+                  if (value == 0) {
+                    return 'Please select the point';
+                  }
+                  return null;
+                },
+              ),
+              Padding(
+                padding: const EdgeInsets.all(16.0),
+                child: Column(
+                  children: [
+                    RadioListTile(
+                        title: Text("Team Leader (+10)"),
+                        value: Team.leader,
+                        groupValue: _team,
+                        onChanged: (value) {
+                          setState(() {
+                            _team = value!;
+                          });
+                        }),
+                    RadioListTile(
+                        title: Text("Team Member"),
+                        value: Team.member,
+                        groupValue: _team,
+                        onChanged: (value) {
+                          setState(() {
+                            _team = value!;
+                          });
+                        }),
+                    Row(
+                      mainAxisAlignment: MainAxisAlignment.spaceBetween,
+                      children: [
+                        Text(
+                          "Absence less than 4",
+                          style: TextStyle(fontSize: 18),
+                        ),
+                        Checkbox(
+                            value: _isAbsense,
+                            onChanged: (value) {
+                              setState(() {
+                                _isAbsense = value!;
+                              });
+                            }),
+                      ],
+                    ),
+                  ],
+                ),
+              ),
+              ElevatedButton(
+                  child: const Text('Enter'),
+                  onPressed: () {
+                    setState(() {
+                      if (_formKey.currentState!.validate()) {
+                        ScaffoldMessenger.of(context).showSnackBar(
+                            const SnackBar(content: Text("processing data")));
+                        _formKey.currentState!.save();
+                        print(studentResult);
+                      }
+                    });
+                  })
+            ],
+          )),
+    );
+  }
+}
+
+class StudentResult {
+  int midTermExam;
+  int finalTermExam;
+  int teamLeaderPoint;
+  int additionalPoint;
+  bool attendance;
+
+  StudentResult(this.midTermExam, this.finalTermExam, this.teamLeaderPoint,
+      this.additionalPoint, this.attendance);
+
+  @override
+  String toString() {
+    return '('
+        '$midTermExam,'
+        '$finalTermExam,'
+        '$teamLeaderPoint,'
+        '$additionalPoint, '
+        '$attendance)';
+  }
+}
