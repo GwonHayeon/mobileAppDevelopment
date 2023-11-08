8bdf27594d471edb6bad18f130d27db1d7990472
hayeon107 <khappy616@gmail.com>
2023-11-09 03:13:35 +0900

    1109 Navigator push ADN pop

--- lib/main.dart
+++ lib/main.dart
@@ -16,7 +16,7 @@ class MyApp extends StatelessWidget {
       //const제거
       title: 'Grade Calculator',
       theme: ThemeData(
-        primarySwatch: Colors.orange, //이것까지 해줘야함
+        primarySwatch: Colors.purple, //이것까지 해줘야함
       ),
       home: MyHomePage(),
     );
@@ -31,6 +31,8 @@ class MyHomePage extends StatefulWidget {
 }
 
 class _MyHomePageState extends State<MyHomePage> {
+  StudentResult studentResult = StudentResult(0, 0, 0, 0, true);
+
   final _midtermController = TextEditingController();
   final _finalController = TextEditingController();
   String _grade = " ";
@@ -58,138 +60,257 @@ class _MyHomePageState extends State<MyHomePage> {
           ),
         ],
       ),
-      body: Padding(
-        padding: const EdgeInsets.all(16.0),
-        child: ListView(
-          scrollDirection: Axis.vertical,
-          children: [
-            TextField(
-              decoration: InputDecoration(
-                border: OutlineInputBorder(),
-                labelText: 'Midterm Exam',
+      body: MyForm(),
+    );
+  }
+}
+
+class MyForm extends StatefulWidget {
+  const MyForm({super.key});
+
+  @override
+  State<MyForm> createState() => _MyFormState();
+}
+
+class _MyFormState extends State<MyForm> {
+  StudentResult studentResult = StudentResult(0, 0, 0, -1, true);
+
+  final _formKey = GlobalKey<FormState>();
+  final _midtermController = TextEditingController();
+  final _finalController = TextEditingController();
+  String _grade = " ";
+  Team _team = Team.leader;
+  final _valueList = List.generate(10, (index) => "$index point");
+  var _selectedValue = '0 point';
+  bool _isAbsense = false;
+
+  @override
+  Widget build(BuildContext context) {
+    return Padding(
+      padding: const EdgeInsets.all(16.0),
+      child: Form(
+          key: _formKey,
+          child: ListView(
+            children: [
+              TextFormField(
+                decoration: const InputDecoration(
+                  border: OutlineInputBorder(),
+                  labelText: 'Mid-term exam',
+                ),
+                validator: (value) {
+                  if (value == null || value.isEmpty) {
+                    return "Insert some text";
+                  } else if (int.tryParse(value) == null) {
+                    return 'Insert some integer';
+                  }
+                  return null;
+                },
+                onSaved: (value) {
+                  studentResult.midTermExam = int.parse(value!);
+                },
               ),
-              controller: _midtermController,
-              keyboardType: TextInputType.number, //키보드 타임 숫자로하기
+              const SizedBox(
+                height: 20,
+              ),
+              TextFormField(
+                decoration: const InputDecoration(
+                  border: OutlineInputBorder(),
+                  labelText: 'Final-term exam',
+                ),
+                validator: (value) {
+                  if (value == null || value.isEmpty) {
+                    return "Insert some text";
+                  } else if (int.tryParse(value) == null) {
+                    return 'Insert some integer';
+                  }
+                  return null;
+                },
+                onSaved: (value) {
+                  studentResult.finalTermExam = int.parse(value!);
+                },
+              ),
+              const SizedBox(
+                height: 20,
+              ),
+              DropdownButtonFormField(
+                decoration: const InputDecoration(
+                    border: OutlineInputBorder(),
+                    labelText: 'Additional Point'),
+                value: studentResult.additionalPoint,
+                items: List.generate(
+                  11,
+                  (i) {
+                    if (i == 0) {
+                      return DropdownMenuItem(
+                        value: i - 1,
+                        child: const Text('Choose the additioinal point'),
+                      );
+                    }
+                    return DropdownMenuItem(
+                      value: i - 1,
+                      child: Text('${i - 1} point'),
+                    );
+                  },
+                ),
+                onChanged: (value) {
+                  setState(() {
+                    studentResult.additionalPoint = value!;
+                  });
+                },
+                validator: (value) {
+                  if (value == -1) {
+                    return 'Please select the point';
+                  }
+
+                  return null;
+                },
+              ),
+              Padding(
+                padding: const EdgeInsets.all(16.0),
+                child: Column(
+                  children: [
+                    RadioListTile(
+                        title: Text("Team Leader (+10)"),
+                        value: Team.leader,
+                        groupValue: _team,
+                        onChanged: (value) {
+                          setState(() {
+                            _team = value!;
+                          });
+                        }),
+                    RadioListTile(
+                        title: Text("Team Member"),
+                        value: Team.member,
+                        groupValue: _team,
+                        onChanged: (value) {
+                          setState(() {
+                            _team = value!;
+                          });
+                        }),
+                    Row(
+                      mainAxisAlignment: MainAxisAlignment.spaceBetween,
+                      children: [
+                        Text(
+                          "Absence less than 4",
+                          style: TextStyle(fontSize: 18),
+                        ),
+                        Checkbox(
+                            value: _isAbsense,
+                            onChanged: (value) {
+                              setState(() {
+                                _isAbsense = value!;
+                              });
+                            }),
+                      ],
+                    ),
+                  ],
+                ),
+              ),
+              ElevatedButton(
+                  child: const Text('Enter'),
+                  onPressed: () {
+                    setState(() {
+                      if (_formKey.currentState!.validate()) {
+                        ScaffoldMessenger.of(context).showSnackBar(
+                            const SnackBar(content: Text("processing data")));
+                        _formKey.currentState!.save();
+                        //Routing
+                        Navigator.push(
+                          context,
+                          MaterialPageRoute(builder: (context) => ResultPage()),
+                        );
+                      }
+                    });
+                  })
+            ],
+          )),
+    );
+  }
+}
+
+class ResultPage extends StatelessWidget {
+  const ResultPage({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      appBar: AppBar(
+        title: Text("Grade Calculator"),
+        actions: [
+          IconButton(
+            onPressed: () {},
+            icon: Icon(Icons.add),
+          ),
+        ],
+      ),
+      body: Center(
+        child: Column(
+          mainAxisAlignment: MainAxisAlignment.center,
+          children: [
+            Text(
+              'Total Sum',
+              style: TextStyle(
+                fontSize: 30,
+              ),
             ),
-            Container(
+            SizedBox(
               height: 20,
             ),
-            TextField(
-              decoration: InputDecoration(
-                border: OutlineInputBorder(),
-                labelText: 'Final Exam',
-              ),
-              controller: _finalController,
-              keyboardType: TextInputType.number,
-            ),
-            RadioListTile(
-                title: Text("Project Team Leader (+10)"),
-                value: Team.leader,
-                groupValue: _team,
-                onChanged: (value) {
-                  setState(() {
-                    _team = value!;
-                  });
-                }),
-            RadioListTile(
-                title: Text("Project Team Member"),
-                value: Team.member,
-                groupValue: _team,
-                onChanged: (value) {
-                  setState(() {
-                    _team = value!;
-                  });
-                }),
-            Padding(
-              padding: const EdgeInsets.all(16.0),
-              child: Column(
-                children: [
-                  Row(
-                    mainAxisAlignment: MainAxisAlignment.spaceBetween,
-                    children: [
-                      Text(
-                        "Additional Point",
-                        style: TextStyle(fontSize: 18),
-                      ),
-                      DropdownButton(
-                          value: _selectedValue,
-                          items: _valueList
-                              .map((points) => DropdownMenuItem(
-                                  value: points,
-                                  child: Text(
-                                    points,
-                                    style: TextStyle(fontSize: 18),
-                                  )))
-                              .toList(),
-                          onChanged: (value) {
-                            setState(() {
-                              _selectedValue = value!;
-                            });
-                          }),
-                    ],
-                  ),
-                  Row(
-                    mainAxisAlignment: MainAxisAlignment.spaceBetween,
-                    children: [
-                      Text(
-                        "Absence less than 4",
-                        style: TextStyle(fontSize: 18),
-                      ),
-                      Checkbox(
-                          value: _isAbsense,
-                          onChanged: (value) {
-                            setState(() {
-                              _isAbsense = value!;
-                            });
-                          }),
-                    ],
-                  ),
-                ],
+            const Text(
+              '0',
+              style: TextStyle(
+                fontSize: 50,
               ),
             ),
-            Center(
-              child: Text(
-                _grade,
-                style: TextStyle(fontSize: 50, color: Colors.red),
-              ),
-            ),
-            Container(
-              height: 10,
+            const SizedBox(
+              height: 20,
             ),
             ElevatedButton(
+              child: Text('Insert Again'),
               onPressed: () {
-                setState(() {
-                  var midtermValue =
-                      double.parse(_midtermController.text.trim());
-                  var finalValue = double.parse(_finalController.text.trim());
-                  var _addValue = double.parse(_selectedValue[0]);
-                  var _bonusValue;
-                  if (_team.toString() == "Team.member") {
-                    _bonusValue = 0;
-                  } else {
-                    _bonusValue = 10;
-                  }
-                  var total =
-                      midtermValue + finalValue + _bonusValue + _addValue;
-                  if (total < 110 || _isAbsense == false) {
-                    _grade = "F";
-                  } else if (total >= 110 && total < 130) {
-                    _grade = "D";
-                  } else if (total >= 130 && total < 150) {
-                    _grade = "C";
-                  } else if (total >= 150 && total < 170) {
-                    _grade = "B";
-                  } else if (total >= 170) {
-                    _grade = "A";
-                  }
-                });
+                Navigator.pop(context);
               },
-              child: Text("Enter", style: TextStyle(fontWeight: FontWeight.bold),),
             )
           ],
         ),
       ),
-      drawer: Drawer(),
     );
+  }
+}
+
+class StudentResult {
+  int midTermExam;
+  int finalTermExam;
+  int teamLeaderPoint;
+  int additionalPoint;
+  bool attendance;
+  int? totalPoint; //null이 될 수도 있음
+
+  StudentResult(
+    this.midTermExam,
+    this.finalTermExam,
+    this.teamLeaderPoint,
+    this.additionalPoint,
+    this.attendance,
+  );
+
+  computeSum() {
+    if (additionalPoint != -1) {
+      totalPoint =
+          midTermExam + finalTermExam + teamLeaderPoint + additionalPoint;
+      if (!attendance) {
+        totalPoint = 0;
+      }
+    }
+  }
+
+  @override
+  String toString() {
+    return '('
+        '$midTermExam,'
+        '$finalTermExam,'
+        '$teamLeaderPoint,'
+        '$additionalPoint, '
+        '$attendance)';
   }
 }
