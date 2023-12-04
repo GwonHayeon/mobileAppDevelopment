693ad0ee388a424b871aac260b2cf44788950522
hayeon107 <khappy616@gmail.com>
2023-12-05 01:06:00 +0900

    1130 video13 chatting

--- lib/LoginPage.dart
+++ lib/LoginPage.dart
@@ -2,6 +2,7 @@ import 'package:flutter/material.dart';
 import 'package:firebase_auth/firebase_auth.dart';
 import 'package:flutterpractice/ChatPage.dart';
 import 'RegisterPage.dart';
+import 'package:modal_progress_hud_nsn/modal_progress_hud_nsn.dart';
 
 class LoginPage extends StatelessWidget {
   const LoginPage({super.key});
@@ -25,72 +26,82 @@ class LoginForm extends StatefulWidget {
 }
 
 class _LoginFormState extends State<LoginForm> {
+  bool saving = false;
   final _authentication = FirebaseAuth.instance;
   final _formKey = GlobalKey<FormState>();
   String email = '';
   String password = '';
-
   @override
-  Widget build(BuildContext context) {
-    return Padding(
-      padding: const EdgeInsets.all(16.0),
-      child: Form(
-        key: _formKey,
-        child: ListView(
-          children: [
-            TextFormField(
-              decoration: const InputDecoration(
-                labelText: 'Email',
-              ),
-              onChanged: (value) {
-                email = value;
-                print(email);
-              },
-            ),
-            const SizedBox(
-              height: 20,
-            ),
-            TextFormField(
-              obscureText: true,
-              decoration: const InputDecoration(labelText: 'Password'),
-              onChanged: (value) {
-                password = value;
-              },
-            ),
-            const SizedBox(
-              height: 20,
-            ),
-            ElevatedButton(
-                onPressed: () async {
-                  try {
-                    final currentUser =
-                    await _authentication.signInWithEmailAndPassword(
-                        email: email, password: password);
 
-                    if (currentUser.user != null) {
-                      _formKey.currentState!.reset();
-                    }
-                  } catch (e) {
-                    print(e);
-                  }
+  Widget build(BuildContext context) {
+    return ModalProgressHUD(
+      inAsyncCall: saving,
+      child: Padding(
+        padding: const EdgeInsets.all(16.0),
+        child: Form(
+          key: _formKey,
+          child: ListView(
+            children: [
+              TextFormField(
+                decoration: const InputDecoration(
+                  labelText: 'Email',
+                ),
+                onChanged: (value) {
+                  email = value;
+                  print(email);
                 },
-                child: Text("Enter")),
-            Row(
-              mainAxisAlignment: MainAxisAlignment.end,
-              children: [
-                Text("If you did not register"),
-                TextButton(
-                  onPressed: () {
-                    Navigator.push(
-                        context,
-                        MaterialPageRoute(
-                            builder: (context) => RegisterPage()));
+              ),
+              const SizedBox(
+                height: 20,
+              ),
+              TextFormField(
+                obscureText: true,
+                decoration: const InputDecoration(labelText: 'Password'),
+                onChanged: (value) {
+                  password = value;
+                },
+              ),
+              const SizedBox(
+                height: 20,
+              ),
+              ElevatedButton(
+                  onPressed: () async {
+                    try {
+                      setState(() {
+                        saving = true;
+                      });
+                      final currentUser =
+                      await _authentication.signInWithEmailAndPassword(
+                          email: email, password: password);
+
+                      if (currentUser.user != null) {
+                        _formKey.currentState!.reset();
+                      }
+                      setState(() {
+                        saving = false;
+                      });
+                    } catch (e) {
+                      print(e);
+                    }
                   },
-                  child: Text('Register your email'),
-                )
-              ],
-            )
-          ],
+                  child: Text("Enter")),
+              Row(
+                mainAxisAlignment: MainAxisAlignment.end,
+                children: [
+                  Text("If you did not register"),
+                  TextButton(
+                    onPressed: () {
+                      Navigator.push(
+                          context,
+                          MaterialPageRoute(
+                              builder: (context) => RegisterPage()));
+                    },
+                    child: Text('Register your email'),
+                  )
+                ],
+              )
+            ],
+          ),
         ),
       ),
     );
--- lib/RegisterPage.dart
+++ lib/RegisterPage.dart
@@ -1,6 +1,8 @@
+import 'package:cloud_firestore/cloud_firestore.dart';
 import 'package:flutter/material.dart';
 import 'package:firebase_auth/firebase_auth.dart';
 import 'package:flutterpractice/SuccessRegister.dart';
+import 'package:modal_progress_hud_nsn/modal_progress_hud_nsn.dart';
 
 class RegisterPage extends StatelessWidget {
   const RegisterPage({super.key});
@@ -24,77 +26,101 @@ class RegisterForm extends StatefulWidget {
 }
 
 class _RegisterFormState extends State<RegisterForm> {
+  bool saving = false;
   final _authentication = FirebaseAuth.instance; //Firebase 인증 소통창구
   final _formKey = GlobalKey<FormState>();
   String email = '';
   String password = '';
+  String userName = '';
 
   @override
   Widget build(BuildContext context) {
-    return Padding(
-      padding: const EdgeInsets.all(16.0),
-      child: Form(
-        key: _formKey,
-        child: ListView(
-          children: [
-            TextFormField(
-              decoration: const InputDecoration(
-                labelText: 'Email',
-              ),
-              onChanged: (value) {
-                email = value;
-                print(email);
-              },
-            ),
-            const SizedBox(
-              height: 20,
-            ),
-            TextFormField(
-              obscureText: true,
-              decoration: const InputDecoration(labelText: 'Password'),
-              onChanged: (value) {
-                password = value;
-              },
-            ),
-            const SizedBox(
-              height: 20,
-            ),
-            ElevatedButton(
-                onPressed: () async {
-                  try {
-                    //email과 password로 새 유저를 등록
-                    final newUser =
-                        await _authentication.createUserWithEmailAndPassword(
-                            email: email, password: password);
-                    if (newUser.user != null) {
-                      _formKey.currentState!.reset(); //null이 아님을 보장
-                      if (!mounted) {
-                        return; //위젯이 트리에 마운트 안될수도 있어서 마운트 안되면 리턴하라고 하는거임. 어싱크를 사용해서그럼
-                      }
-                      Navigator.push(
-                          context,
-                          MaterialPageRoute(
-                              builder: (context) =>
-                                  const SuccessRegisterPage()));
-                    }
-                  } catch (e) {
-                    print(e);
-                  }
+    return ModalProgressHUD(
+      inAsyncCall: saving,
+      child: Padding(
+        padding: const EdgeInsets.all(16.0),
+        child: Form(
+          key: _formKey,
+          child: ListView(
+            children: [
+              TextFormField(
+                decoration: const InputDecoration(
+                  labelText: 'Email',
+                ),
+                onChanged: (value) {
+                  email = value;
+                  print(email);
                 },
-                child: Text("Register")),
-            Row(
-              mainAxisAlignment: MainAxisAlignment.end,
-              children: [
-                Text("If you already registered"),
-                TextButton(
-                  onPressed: () {
-                    Navigator.pop(context);
+              ),
+              const SizedBox(
+                height: 20,
+              ),
+              TextFormField(
+                obscureText: true,
+                decoration: const InputDecoration(labelText: 'Password'),
+                onChanged: (value) {
+                  password = value;
+                },
+              ),
+              const SizedBox(
+                height: 20,
+              ),
+              TextFormField(
+                decoration: const InputDecoration(labelText: 'User name'),
+                onChanged: (value) {
+                  userName = value;
+                },
+              ),
+              const SizedBox(
+                height: 20,
+              ),
+              ElevatedButton(
+                  onPressed: () async {
+                    try {
+                      setState(() {
+                        saving = true;
+                      });
+                      //email과 password로 새 유저를 등록
+                      final newUser =
+                          await _authentication.createUserWithEmailAndPassword(
+                              email: email, password: password);
+                      await FirebaseFirestore.instance.collection('user').doc(newUser.user!.uid).set({ //비동기
+                        'userName':userName,
+                        'email' : email,
+                      });
+                      if (newUser.user != null) {
+                        _formKey.currentState!.reset(); //null이 아님을 보장
+                        if (!mounted) {
+                          return; //위젯이 트리에 마운트 안될수도 있어서 마운트 안되면 리턴하라고 하는거임. 어싱크를 사용해서그럼
+                        }
+                        Navigator.push(
+                            context,
+                            MaterialPageRoute(
+                                builder: (context) =>
+                                    const SuccessRegisterPage()));
+                        setState(() {
+                          saving = false;
+                        });
+                      }
+                    } catch (e) {
+                      print(e);
+                    }
                   },
-                  child: Text('Log in with your email'),
-                )
-              ],
-            )
-          ],
+                  child: Text("Register")),
+              Row(
+                mainAxisAlignment: MainAxisAlignment.end,
+                children: [
+                  Text("If you already registered"),
+                  TextButton(
+                    onPressed: () {
+                      Navigator.pop(context);
+                    },
+                    child: Text('Log in with your email'),
+                  )
+                ],
+              )
+            ],
+          ),
         ),
       ),
     );
